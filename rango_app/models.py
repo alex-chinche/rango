037661c9d61682c0c00bbc3a7a4d6da35feba6a9
Alex <alexrodriguez1995.arg@gmail.com>
2020-10-08 11:38:35 +0200

    populate file added

--- rango_app/models.py
+++ rango_app/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    class Meta: verbose_name_plural = 'Categories'
 
-def __str__(self):
-    return self.name
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
 
 
 class Page(models.Model):
