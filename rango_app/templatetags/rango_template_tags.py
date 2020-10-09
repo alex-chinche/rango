d4953016ffd1c22d52d8ec1928dd9d31e9fcc453
Alex <alexrodriguez1995.arg@gmail.com>
2020-10-09 17:39:35 +0200

    template_tags done

--- rango_app/templatetags/rango_template_tags.py
+++ rango_app/templatetags/rango_template_tags.py
@@ -6,7 +6,4 @@ register = template.Library()
 
 @register.inclusion_tag('categories.html')
 def get_category_list(current_category=None):
-    return {
-        'categories': Category.objects.all(),
-        'current_category': current_category
-    }
+    return {'categories': Category.objects.all()}
